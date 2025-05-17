0a3b7b8e7609b88744526ba96569862ef6cb3c85
dhruvag05 <dhruv.agarwal@magicbricks.com>
2025-05-18 01:11:51 +0530

    Configure Next.js for xlsx module

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
