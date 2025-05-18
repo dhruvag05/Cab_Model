6b56c5d5d364072d378408ad4925b8d5bafb22ac
dhruvag05 <dhruv.agarwal@magicbricks.com>
2025-05-18 13:51:41 +0530

    Update AssemblyAI package references

--- lib/analyze-audio.ts
+++ lib/analyze-audio.ts
@@ -1,4 +1,4 @@
-import { AssemblyAI } from '@assemblyai/core';
+import { AssemblyAI } from 'assemblyai';
 
 const client = new AssemblyAI({
   apiKey: process.env.NEXT_PUBLIC_ASSEMBLYAI_API_KEY || ''
